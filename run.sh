551a4eca32369d982de60bc6ebed4e2482667ff5
sycured <60801403+sycured@users.noreply.github.com>
2020-10-01 19:50:55 -0500

    migrate from archlinux to alpine to reduce size

--- run.sh
+++ run.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 set -x
-mkimg=$(buildah from archlinux)
-buildah config --author='sycured' $mkimg
-buildah config --label Name='latex-builder' $mkimg
-buildah run "$mkimg" -- pacman -Syyu --noconfirm
-buildah run "$mkimg" -- pacman -S --noconfirm biber cmake ghostscript grep ninja tar texlive-latexextra texlive-fontsextra python-pip
+mkimg=$(buildah from alpine)
+buildah config --author='sycured' "$mkimg"
+buildah config --label Name='latex-builder' "$mkimg"
+buildah run "$mkimg" -- apk update
+buildah run "$mkimg" -- apk upgrade
+buildah run "$mkimg" -- apk add bash biber cmake ghostscript ninja py3-pip texlive texmf-dist-latexextra texmf-dist-fontsextra
 buildah run "$mkimg" -- pip install --no-cache-dir blacktex
-buildah run "$mkimg" -- pacman -Scc --noconfirm
-buildah run "$mkimg" -- ln -s /usr/bin/vendor_perl/biber /usr/bin/biber
 buildah run "$mkimg" -- mkdir /shared
-buildah config --workingdir='/shared' $mkimg
-buildah config --entrypoint='/shared/run.sh' $mkimg
-mntimg=$(buildah mount $mkimg)
-rm -rf $mntimg/var/cache/pacman/pkg/*
+buildah config --workingdir='/shared' "$mkimg"
+buildah config --entrypoint='/shared/run.sh' "$mkimg"
+mntimg=$(buildah mount "$mkimg")
+rm -rf "$mntimg"/var/cache/apk/*
 git clone https://gitlab.kitware.com/kmorel/UseLATEX.git
-mv UseLATEX/UseLATEX.cmake $mntimg/usr/share/cmake-*/Modules/
+mv UseLATEX/UseLATEX.cmake "$mntimg"/usr/share/cmake/Modules/
 git clone https://github.com/sycured/pdfcompressor.git
-mv pdfcompressor/pdfcompressor $mntimg/usr/local/bin/
-chmod 555 $mntimg/usr/local/bin/pdfcompressor
-buildah unmount $mkimg
+mv pdfcompressor/pdfcompressor "$mntimg"/usr/local/bin/
+chmod 555 "$mntimg"/usr/local/bin/pdfcompressor
+buildah unmount "$mkimg"
 buildah commit --squash "$mkimg" "latex-builder"
 buildah rm "$mkimg"
