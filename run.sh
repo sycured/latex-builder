bf93f309042612a3038ea6b6139694a5bec704c9
sycured <60801403+sycured@users.noreply.github.com>
2021-02-24 15:58:31 -0500

    add luatex + lexend + remove ci cron + migrate to fedora

--- run.sh
+++ run.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 set -x
 
-mkbuild=$(buildah from alpine)
-buildah run "$mkbuild" -- apk update
-buildah run "$mkbuild" -- apk upgrade
-buildah run "$mkbuild" -- apk add autoconf automake gcc git libc-dev libtool make
-buildah run "$mkbuild" -- git clone https://git.savannah.gnu.org/git/chktex.git
-buildah run "$mkbuild" -- bash -c "mkdir -p /usr/local/etc && cd chktex/chktex && sh autogen.sh --prefix=/usr/bin && ./configure && make && install chktex /usr/bin && install chktexrc /usr/local/etc"
-mntbuild=$(buildah mount "$mkbuild")
-
-mkimg=$(buildah from python:alpine)
+mkimg=$(buildah from fedora:rawhide)
 buildah config --author='sycured' "$mkimg"
 buildah config --label Name='latex-builder' "$mkimg"
-buildah run "$mkimg" -- apk update
-buildah run "$mkimg" -- apk upgrade
-buildah run "$mkimg" -- apk add bash biber cmake coreutils ghostscript ninja py3-pip rclone texlive texlive-dvi texmf-dist-latexextra texmf-dist-fontsextra
+buildah run "$mkimg" -- dnf upgrade -y
+buildah run "$mkimg" -- dnf install -y biber cmake ghostscript ninja-build python3-pip rclone texlive texlive-chktex texlive-luatex texlive-collection-latexextra texlive-collection-fontsextra
 buildah run "$mkimg" -- pip install --no-cache-dir blacktex
-buildah run "$mkimg" -- mkdir -p /usr/local/etc
 mntimg=$(buildah mount "$mkimg")
-rm -rf "$mntimg"/var/cache/apk/*
-cp "$mntbuild"/usr/bin/chktex "$mntimg"/usr/bin/chktex
-cp "$mntbuild"/usr/local/etc/chktexrc "$mntimg"/usr/local/etc/chktexrc
-chmod 555 "$mntimg"/usr/bin/chktex
-buildah unmount "$mkbuild"
-buildah rm "$mkbuild"
+rm -rf "$mntimg"/var/cache/dnf/*
 git clone https://gitlab.kitware.com/kmorel/UseLATEX.git
 mv UseLATEX/UseLATEX.cmake "$mntimg"/usr/share/cmake/Modules/
 git clone https://github.com/sycured/pdfcompressor.git
