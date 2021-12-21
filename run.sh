722f5b3aafba19a0a4403c03af1135d0b5fc1335
sycured <60801403+sycured@users.noreply.github.com>
2021-12-21 09:03:34 -0500

    add gzip and tar to remove complaint from uselatex

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -x
 mkimg=$(buildah from fedora-minimal:rawhide)
 buildah config --author="sycured" --label Name="latex-builder" --label org.opencontainers.image.source="https://github.com/sycured/latex-builder" "$mkimg"
 buildah run "$mkimg" -- microdnf upgrade -y
-buildah run "$mkimg" -- microdnf --nodocs --setopt=install_weak_deps=0 --best install -y cmake ghostscript ninja-build rclone texlive texlive-chktex texlive-luatex texlive-collection-latexextra texlive-collection-fontsextra
+buildah run "$mkimg" -- microdnf --nodocs --setopt=install_weak_deps=0 --best install -y cmake ghostscript gzip ninja-build rclone tar texlive texlive-chktex texlive-luatex texlive-collection-latexextra texlive-collection-fontsextra
 buildah run "$mkimg" -- useradd -ms /bin/bash latex
 mntimg=$(buildah mount "$mkimg")
 rm -rf "$mntimg"/var/cache/dnf/*
