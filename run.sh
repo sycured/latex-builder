e13d4653d438f477c8144804971bf646a2e815d5
sycured <60801403+sycured@users.noreply.github.com>
2023-01-19 21:09:24 -0500

    migrate to openSUSE Tumbleweed

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -x
 
-mkimg=$(buildah from fedora-minimal:rawhide)
+mkimg=$(buildah from registry.opensuse.org/opensuse/tumbleweed:latest)
 buildah config --author="sycured" --label Name="latex-builder" --label org.opencontainers.image.source="https://github.com/sycured/latex-builder" "${mkimg}"
-buildah run "${mkimg}" -- microdnf upgrade -y
-buildah run "${mkimg}" -- microdnf --nodocs --setopt=install_weak_deps=0 --best install -y cmake ghostscript git gzip perl-Image-ExifTool poppler-utils qpdf rclone samurai tar texlive texlive-chktex texlive-luatex texlive-collection-latexextra texlive-collection-fontsextra
-buildah run "${mkimg}" -- microdnf remove microdnf libdnf -y
+buildah run "${mkimg}" -- bash -c 'echo "download.min_download_speed=10000000" >> /etc/zypp/zypp.conf'
+buildah run "${mkimg}" -- zypper update -y
+buildah run "${mkimg}" -- zypper install -y cmake ghostscript git gzip perl-Image-ExifTool poppler-tools qpdf rclone samurai shadow tar texlive texlive-chktex texlive-luatex texlive-collection-latexextra texlive-collection-fontsextra
 buildah run "${mkimg}" -- useradd -ms /bin/bash latex
 buildah run "${mkimg}" -- mkdir -p /home/latex/.config/rclone
 buildah run "${mkimg}" -- chown -R latex:latex /home/latex/.config
 mntimg=$(buildah mount "${mkimg}")
-rm -rf "${mntimg}"/var/lib/dnf/*
-rm -rf "${mntimg}"/var/cache/yum/*
+rm -rf "${mntimg}"/var/lib/rpm/*
+rm -rf "${mntimg}"/var/cache/zypp/*
 git clone https://gitlab.kitware.com/kmorel/UseLATEX.git
 mv UseLATEX/UseLATEX.cmake "${mntimg}"/usr/share/cmake/Modules/
 mv entrypoint.sh "${mntimg}"/
