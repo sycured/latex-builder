a10cd08fb8318b023ebe4c5e1f7071ea4852cd27
sycured <60801403+sycured@users.noreply.github.com>
2023-05-06 22:50:00 -0500

    add texlive-lexend

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ mkimg=$(buildah from registry.opensuse.org/opensuse/tumbleweed:latest)
 buildah config --author="sycured" --label Name="latex-builder" --label org.opencontainers.image.source="https://github.com/sycured/latex-builder" "${mkimg}"
 buildah run "${mkimg}" -- bash -c 'echo "download.min_download_speed=10000000" >> /etc/zypp/zypp.conf'
 buildah run "${mkimg}" -- zypper update -y
-buildah run "${mkimg}" -- zypper install -y cmake ghostscript git gzip perl-Image-ExifTool poppler-tools qpdf rclone samurai shadow tar texlive texlive-chktex texlive-luatex texlive-collection-latexextra texlive-collection-fontsextra
+buildah run "${mkimg}" -- zypper install -y cmake ghostscript git gzip perl-Image-ExifTool poppler-tools qpdf rclone samurai shadow tar texlive texlive-chktex texlive-luatex texlive-collection-latexextra texlive-collection-fontsextra texlive-lexend
 buildah run "${mkimg}" -- useradd -ms /bin/bash latex
 buildah run "${mkimg}" -- mkdir -p /home/latex/.config/rclone
 buildah run "${mkimg}" -- chown -R latex:latex /home/latex/.config
